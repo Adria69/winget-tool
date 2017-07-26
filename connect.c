c1f2bee5efb67c356aa2a467287012cd96d22973
Junio C Hamano <junio@pobox.com>
2017-07-26 10:24:20 -0700

    connect: reject ssh hostname that begins with a dash
    When commands like "git fetch" talk with ssh://$rest_of_URL/, the
    code splits $rest_of_URL into components like host, port, etc., and
    then spawns the underlying "ssh" program by formulating argv[] array
    that has:
    
     - the path to ssh command taken from GIT_SSH_COMMAND, etc.
    
     - dashed options like '-batch' (for Tortoise), '-p <port>' as
       needed.
    
     - ssh_host, which is supposed to be the hostname parsed out of
       $rest_of_URL.
    
     - then the command to be run on the other side, e.g. git
       upload-pack.
    
    If the ssh_host ends up getting '-<anything>', the argv[] that is
    used to spawn the command becomes something like:
    
        { "ssh", "-p", "22", "-<anything>", "command", "to", "run", NULL }
    
    which obviously is bogus, but depending on the actual value of
    "<anything>", will make "ssh" parse and use it as an option.
    
    Prevent this by forbidding ssh_host that begins with a "-".
    
    [jes: adjustes lib-proto-disable.sh to unset proto.allow=never]
    
    Noticed-by: Joern Schneeweisz of Recurity Labs
    Reported-by: Brian at GitLab
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Reviewed-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- connect.c
+++ connect.c
@@ -791,6 +791,9 @@ struct child_process *git_connect(int fd[2], const char *url,
 				return NULL;
 			}
 
+			if (ssh_host[0] == '-')
+				die("strange hostname '%s' blocked", ssh_host);
+
 			ssh = get_ssh_command();
 			if (!ssh) {
 				const char *base;
