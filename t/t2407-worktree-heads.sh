a58d55b2fdba39baa8a29c4e37cc2c6a5f38d918
Derrick Stolee <derrickstolee@github.com>
2022-06-08 20:09:00 +0000

    branch: check for bisects and rebases
    The branch_checked_out() helper was added by the previous change, but it
    used an over-simplified view to check if a branch is checked out. It
    only focused on the HEAD symref, but ignored whether a bisect or rebase
    was happening.
    
    Teach branch_checked_out() to check for these things, and also add tests
    to ensure that we do not lose this functionality in the future.
    
    Now that this test coverage exists, we can safely refactor
    validate_new_branchname() to use branch_checked_out().
    
    Note that we need to prepend "refs/heads/" to the 'state.branch' after
    calling wt_status_check_*(). We also need to duplicate wt->path so the
    value is not freed at the end of the call.
    
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2407-worktree-heads.sh
+++ t/t2407-worktree-heads.sh
@@ -21,4 +21,33 @@ test_expect_success 'refuse to overwrite: checked out in worktree' '
 	done
 '
 
+test_expect_success 'refuse to overwrite: worktree in bisect' '
+	test_when_finished test_might_fail git -C wt-4 bisect reset &&
+
+	(
+		git -C wt-4 bisect start &&
+		git -C wt-4 bisect bad HEAD &&
+		git -C wt-4 bisect good HEAD~3
+	) &&
+
+	test_must_fail git branch -f wt-4 HEAD 2>err &&
+	grep "cannot force update the branch '\''wt-4'\'' checked out at" err
+'
+
+. "$TEST_DIRECTORY"/lib-rebase.sh
+
+test_expect_success 'refuse to overwrite: worktree in rebase' '
+	test_when_finished test_might_fail git -C wt-4 rebase --abort &&
+
+	(
+		set_fake_editor &&
+		FAKE_LINES="edit 1 2 3" \
+			git -C wt-4 rebase -i HEAD~3 >rebase &&
+		git -C wt-4 status
+	) &&
+
+	test_must_fail git branch -f wt-4 HEAD 2>err &&
+	grep "cannot force update the branch '\''wt-4'\'' checked out at" err
+'
+
 test_done
